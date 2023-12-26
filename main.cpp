a8496792a78a54162ebc0a004673cab8ae2faf5f
apilifogladif <filipajfidalgo@gmail.com>
2023-12-26 18:47:23 +0000

    main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << endl << "----------------------------" << endl;
+    cout << endl << "      Main Menu   " << endl;
+    cout << endl << "----------------------------" << endl;
+    cout << "1 - Number of airports." << endl; // i
+    cout << "2 - Number of flights..." << endl; // i, ii, iii
+    cout << "3 - Number of ";
+    system("exit");
+    return 0;
+}

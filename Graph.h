5c01e002bc61c1d5e5e0b2e91a5fcc4e170b73f5
Leonor2004 <up202205796@up.pt>
2023-12-30 12:47:15 +0000

    Complexity comments

--- Graph.h
+++ Graph.h
@@ -307,11 +307,11 @@ public:
      * @return The number of flights associated with the airline.
      */
     int getNumberOfFlights(const Airline& airline) const;
- ////////////////////////////////////////////////////
+
     /**
      * @brief Gets the number of destinations served by the given airline.
      *
-     * Complexity: O
+     * Complexity: O(n^2)
      *
      * @param airline : The airline for which to retrieve the number of destinations.
      * @return A vector of destinations served by the airline.
@@ -321,143 +321,203 @@ public:
 
     /**
      * @brief Gets the number of flights associated with the given city.
-     * @param city The city for which to retrieve the number of flights.
-     * @param country The country of the city.
+     *
+     * Complexity: O(n)
+     *
+     * @param city : The city for which to retrieve the number of flights.
+     * @param country : The country of the city.
      * @return The number of flights associated with the city.
      */
     int getNumberOfFlightsInCity(const string& city, const string& country) const;
 
     /**
      * @brief Gets the number of airports in the specified city.
-     * @param city The city for which to retrieve the number of airports.
-     * @param country The country of the city.
+     *
+     * Complexity: O(n)
+     *
+     * @param city : The city for which to retrieve the number of airports.
+     * @param country : The country of the city.
      * @return The number of unique airports in the city.
      */
     int getNumberOfAirportsInCity(const string& city, const string& country) const;
 
     /**
      * @brief Gets the airports in the specified city.
-     * @param city The city for which to retrieve the number of airports.
-     * @param country The country of the city.
+     *
+     * Complexity: O(n)
+     *
+     * @param city : The city for which to retrieve the number of airports.
+     * @param country : The country of the city.
      * @return The vector of unique airports in the city.
      */
     vector<Airport> getAirportsInCity(const string& city, const string& country) const;
 
     /**
      * @brief Gets the number of unique airlines operating in the specified city.
-     * @param city The city for which to retrieve the number of unique airlines.
-     * @param country The country of the city.
+     *
+     * Complexity: O(n^2)
+     *
+     * @param city : The city for which to retrieve the number of unique airlines.
+     * @param country : The country of the city.
      * @return The number of unique airlines operating in the city.
      */
     int getNumberOfAirlinesInCity(const string& city, const string& country) const;
 
     /**
      * @brief Gets the total number of unique destinations served by airports in the specified city.
-     * @param city The city for which to retrieve the total number of destinations.
-     * @param country The country of the city.
+     *
+     * Complexity: O(n^2)
+     *
+     * @param city : The city for which to retrieve the total number of destinations.
+     * @param country : The country of the city.
      * @return The total number of unique destinations served by airports in the city.
      */
     int getTotalDestinationsInCity(const string& city, const string& country) const;
 
     /**
      * @brief Gets the reachable destinations from airports in the specified city within a given number of stops.
-     * @param city The city for which to retrieve the reachable destinations.
-     * @param country The country of the city.
-     * @param maxStops The maximum number of stops allowed.
+     *
+     * Complexity: O(n)
+     *
+     * @param city : The city for which to retrieve the reachable destinations.
+     * @param country : The country of the city.
+     * @param maxStops : The maximum number of stops allowed.
      * @return Vector of airports representing the reachable destinations.
      */
     vector<Airport> getReachableDestinationsInCity(const string& city, const string& country, int maxStops) const;
 
     /**
      * @brief Gets the total number of flights departing from airports in the specified country.
-     * @param country The country for which to retrieve the number of flights.
+     *
+     * Complexity: O(n)
+     *
+     * @param country : The country for which to retrieve the number of flights.
      * @return The total number of flights departing from airports in the country.
      */
     int getNumberOfFlightsInCountry(const string& country) const;
 
     /**
      * @brief Gets the number of airports in the specified country.
-     * @param country The country for which to retrieve the number of airports.
+     *
+     * Complexity: O(n)
+     *
+     * @param country : The country for which to retrieve the number of airports.
      * @return The number of airports in the country.
      */
     int getNumberOfAirportsInCountry(const string& country) const;
 
     /**
      * @brief Gets the number of unique airlines operating in the specified country.
-     * @param country The country for which to retrieve the number of airlines.
+     *
+     * Complexity: O(n^2)
+     *
+     * @param country : The country for which to retrieve the number of airlines.
      * @return The number of unique airlines in the country.
      */
     int getNumberOfAirlinesInCountry(const string& country) const;
 
     /**
      * @brief Gets the total number of unique destinations in the specified country.
-     * @param country The country for which to retrieve the number of destinations.
+     *
+     * Complexity: O(n^2)
+     *
+     * @param country : The country for which to retrieve the number of destinations.
      * @return The total number of unique destinations in the country.
      */
     int getTotalDestinationsInCountry(const string& country) const;
 
     /**
      * @brief Gets the number of unique cities in the specified country.
-     * @param country The country for which to retrieve the number of cities.
+     *
+     * Complexity: O(n)
+     *
+     * @param country : The country for which to retrieve the number of cities.
      * @return The number of unique cities in the country.
      */
     int getNumberOfCitiesInCountry(const string& country) const;
 
     /**
      * @brief Gets the reachable destinations in the specified country within a given number of stops.
-     * @param country The country of interest.
-     * @param maxStops The maximum number of stops allowed.
+     *
+     * Complexity: O(n)
+     *
+     * @param country : The country of interest.
+     * @param maxStops : The maximum number of stops allowed.
      * @return Vector of airports representing the reachable destinations.
      */
     vector<Airport> getReachableDestinationsInCountry(const string& country, int maxStops) const;
 
     /**
      * @brief Gets the total number of flights in the graph.
+     *
+     * Complexity: O(n)
+     *
      * @return The total number of flights in the graph.
      */
     int getTotalNumberOfFlights() const;
 
     /**
      * @brief Gets the total number of airports in the graph.
+     *
+     * Complexity: O(1)
+     *
      * @return The total number of airports in the graph.
      */
     int getTotalNumberOfAirports() const;
 
     /**
      * @brief Gets the total number of unique airlines operating in the graph.
+     *
+     * Complexity: O(n^2)
+     *
      * @return The total number of unique airlines operating in the graph.
      */
     int getTotalNumberOfAirlines() const;
 
     /**
      * @brief Gets the total number of unique countries in the graph.
+     *
+     * Complexity: O(n)
+     *
      * @return The total number of unique countries in the graph.
      */
     int getTotalNumberOfCountries() const;
 
     /**
      * @brief Gets the total number of unique cities in the graph.
+     *
+     * Complexity: O(n)
+     *
      * @return The total number of unique cities in the graph.
      */
     int getTotalNumberOfCities() const;
 
     /**
      * @brief Gets the total number of reachable destinations within a maximum number of stops in the graph.
-     * @param maxStops The maximum number of stops allowed.
+     *
+     * Complexity: O(n)
+     *
+     * @param maxStops : The maximum number of stops allowed.
      * @return The total number of reachable destinations within the specified number of stops.
      */
     int getTotalReachableDestinations(int maxStops) const;
 
     /**
      * @brief Gets a vector of airlines associated with a specific airport.
-     * @param airport The airport for which to retrieve the associated airlines.
+     *
+     * Complexity: O(n)
+     *
+     * @param airport : The airport for which to retrieve the associated airlines.
      * @return A vector of Airline objects representing the airlines associated with the airport.
      */
     vector<Airline> getAirlines(Airport airport);
 
     /**
      * @brief Gets the top airports based on the number of flights.
-     * @param k The number of top airports to retrieve.
+     *
+     * Complexity: O(n)
+     *
+     * @param k : The number of top airports to retrieve.
      * @return A vector of pairs representing the top airports and their respective number of flights.
      */
     vector<pair<Airport, int>> getTopKAirports(int k) const;
@@ -465,21 +525,29 @@ public:
     /**
      * @brief Finds the maximum stops trip and corresponding source-destination airport pairs, using breadth-first search (BFS)
      * The result is printed to the terminal.
+     *
+     * Complexity: O(n^3)
      */
     void findMaxStopsTrip();
 
     /**
      * @brief Gets airports closest to the specified coordinates.
-     * @param lat Latitude of the target location in degrees.
-     * @param longi Longitude of the target location in degrees.
+     *
+     * Complexity: O(n)
+     *
+     * @param lat : Latitude of the target location in degrees.
+     * @param longi : Longitude of the target location in degrees.
      * @return Vector of airports closest to the specified coordinates.
      */
     vector<Airport> getAirportsInCoordinates(float lat, float longi) const;
 
     /**
      * @brief Finds a path between two airports in the graph using depth-first search (DFS).
-     * @param s Source airport.
-     * @param d Destination airport.
+     *
+     * Complexity: O(n)
+     *
+     * @param s : Source airport.
+     * @param d : Destination airport.
      * @return Vector of airports representing the path from source to destination.
      */
     vector<Airport> pathAirport(Airport s, Airport d);

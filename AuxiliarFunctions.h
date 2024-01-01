10dd98bc036cab5fe0a1ee2f769399ccaba81ca8
g3raldes <g3raldes@gmail.com>
2024-01-01 19:44:34 +0000

    Added some comments

--- AuxiliarFunctions.h
+++ AuxiliarFunctions.h
@@ -11,7 +11,7 @@ public:
     /**
      * @brief Finds articulation points in the given graph using DFS (depth-first search).
      *
-     * Complexity:
+     * Complexity: O(n)
      *
      * @param g : Pointer to the graph.
      * @return Vector of unique airports representing articulation points.
@@ -21,7 +21,7 @@ public:
     /**
      * @brief Find the best flight options between source and destination airports.
      *
-     * Complexity:
+     * Complexity: O(n^3)
      *
      * @param sourceAirports : Vector of source airports.
      * @param destAirports : Vector of destination airports.
@@ -29,8 +29,27 @@ public:
      */
     static vector<vector<Airport>> bestFlightOp(const vector<Airport>& sourceAirports, const vector<Airport>& destAirports);
 
+    /**
+     * @brief Finds the best flight paths between source and destination airports with airline restrictions.
+     *
+     * Complexity: O(n^3)
+     *
+     * @param sourceAirports : Vector of source airports.
+     * @param destAirports : Vector of destination airports.
+     * @param airlines : Vector of airline codes for restrictions.
+     * @return Vector of vectors representing the best flight paths according to the user input.
+     */
     static vector<vector<Airport>> filterVectorAirlines(const vector<Airport>& sourceAirports, const vector<Airport>& destAirports, const vector<string>& airlines);
 
+    /**
+     * @brief Finds the best flight paths between source and destination airports with a specified number of airlines.
+     *
+     * Complexity: O(n^3)
+     * @param sourceAirports : Vector of source airports.
+     * @param destAirports : Vector of destination airports.
+     * @param numAir : Number of airlines for restrictions.
+     * @return Vector of vectors representing the best flight paths according to the user input.
+     */
     static vector<vector<Airport>> filterNumAirlines(const vector<Airport>& sourceAirports, const vector<Airport>& destAirports, int numAir);
 };
 
